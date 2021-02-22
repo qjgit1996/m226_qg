8b6f5c2cc10a2d5356dc008c1bf346ee83bf1902
qjgit1996 <quinten.groenveld@hotmail.com>
2021-02-22 12:55:52 +0100

    ab08

--- src/main/java/m226_1/t1_hello_world/HelloWorld.java
+++ src/main/java/m226_1/t1_hello_world/HelloWorld.java
@@ -2,6 +2,10 @@ package m226_1.t1_hello_world;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int a = 4;
+        double d = 10.0;
+        boolean b = true;
+        char c = 'y';
+        System.out.println(((char) (c+1)));
     }
 }
